82f2b64fc9cb9721c9d451013878a602f9c977c2
Vagner <vagnersaire22@gmail.com>
2025-06-27 21:53:57 -0300

    A ultima Aula de PAM_ Postagens

---
+++ Postagens/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postagens.Models
+{
+    public class PostModel
+    {
+        public int UserId;
+        public int Id;
+        public string Title;
+        public string Body;
+    }
+}
---
+++ Postagens/ViewModels/PostViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Postagens.Models;
+using Postagens.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Postagens.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        List<PostModel> postagens;
+
+        [ObservableProperty]
+        private string titulo;
+
+        [ObservableProperty]
+        private string corpo;
+
+        public ICommand DisplayPostsCommand { get; private set; }
+        public PostViewModel() {
+        
+            DisplayPostsCommand = new Command(DisplayPosts);
+        }
+
+        public async void DisplayPosts()
+        {
+            PostsService postsservice = new PostsService();
+            postagens = await postsservice.getPosts();
+            Titulo = postagens[0].Title;
+            Corpo = postagens[0].Body;
+        }
+    }
+}
